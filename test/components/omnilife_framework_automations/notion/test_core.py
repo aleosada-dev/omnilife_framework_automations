1e9101eaea6e94b2a84e684426faeaf6e9ebdbfa
Alexandre Osada <alexandre.osada.dev@gmail.com>
2024-02-28 20:07:29 -0300

    fix: exclusão da API Key do código fonte

--- test/components/omnilife_framework_automations/notion/test_core.py
+++ test/components/omnilife_framework_automations/notion/test_core.py
@@ -1,18 +1,6 @@
 import pytest
 from omnilife_framework_automations.notion.core import query_pages
 
-def test_query_pages():
-    # Arrange
-    database_id = "72facfe731bd4c79a85c1ea132d59410"
-    api_key = "secret_Gdu1D1v96dtAgIoiCKpFnGRCWjNYwuV9AJBJKnGzkc1"
-
-    # Call the function
-    pages = query_pages(database_id, api_key)
-
-    # Assert the expected output
-    assert isinstance(pages, list)
-    assert all(isinstance(page, dict) for page in pages)
-
 
 @pytest.mark.parametrize("database_id, api_key, expected_error_msg", [
     (None, None, "Database ID is empty or None"),
