4945b41749b0c42f2da205d65176ebb99ef7bb63
Alexandre Osada <alexandre.osada.dev@gmail.com>
2024-03-05 12:44:48 -0300

    feat: inclusão de mensagem de log em caso de erro na lambda de urgent_project

--- bases/omnilife_framework_automations/autofill_urgent_project_lambda/lambda_function.py
+++ bases/omnilife_framework_automations/autofill_urgent_project_lambda/lambda_function.py
@@ -1,14 +1,24 @@
 import os
 import pendulum
 from omnilife_framework_automations.project.automations import urgent_project_automation
+from omnilife_framework_automations.logger.logger import setup_logger
 
 
 def lambda_handler(event, context):
-    database_id = os.environ.get("NOTION_DATABASE_ID")
-    api_key = os.environ.get("NOTION_API_KEY")
+    logger = setup_logger(__name__)
 
-    urgent_project_automation(database_id, api_key, pendulum.now())
+    try:
+        database_id = os.environ.get("NOTION_DATABASE_ID")
+        api_key = os.environ.get("NOTION_API_KEY")
 
-    return {
-        "statusCode": 200,
-    }
+        urgent_project_automation(database_id, api_key, pendulum.now())
+    except Exception as e:
+        logger.error(
+            f"An error occurred: {e}",
+            extra={
+                "params": {
+                    "database_id": database_id,
+                }
+            },
+        )
+        raise
