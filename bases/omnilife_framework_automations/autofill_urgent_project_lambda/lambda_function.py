2b015a50cb265aff9c74b7266ef63668009ac79d
Alexandre Osada <alexandre.osada.dev@gmail.com>
2024-03-11 11:55:10 -0300

    fix: Erro ao na configuração do injector para o módulo de repositório

--- bases/omnilife_framework_automations/autofill_urgent_project_lambda/lambda_function.py
+++ bases/omnilife_framework_automations/autofill_urgent_project_lambda/lambda_function.py
@@ -1,21 +1,24 @@
 import os
 from typing import Self
 from injector import Injector, inject
-from omnilife_framework_automations.project.modules import ProjectServiceModule
+from omnilife_framework_automations.project.modules import (
+    ProjectRepositoryModule,
+    ProjectServiceModule,
+)
 from omnilife_framework_automations.infrastructure.services import IProjectService
-from omnilife_framework_automations.project.repositories import ProjectNotionRepository
 import pendulum
 from omnilife_framework_automations.logger.logger import setup_logger
 
 
 def setup_injector():
-    return Injector([ProjectServiceModule(), ProjectNotionRepository()])
+    return Injector([ProjectServiceModule(), ProjectRepositoryModule()])
+
 
 class Application:
     @inject
     def __init__(self, project_service: IProjectService):
         self.project_service = project_service
-    
+
     def run(self: Self, database_id: str, api_key: str, now: pendulum.DateTime):
         self.project_service.urgent_project_automation(database_id, api_key, now)
 
@@ -27,7 +30,7 @@ def lambda_handler(event, context):
         database_id = os.environ.get("NOTION_DATABASE_ID")
         api_key = os.environ.get("NOTION_API_KEY")
 
-        injector = setup_injector() 
+        injector = setup_injector()
         app = injector.get(Application)
         app.run(database_id, api_key, pendulum.now())
     except Exception as e:
